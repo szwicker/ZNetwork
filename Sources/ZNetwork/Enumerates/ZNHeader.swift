7ae67093e314e7f9e34912c2104e7a9046b9cf37
Simon Zwicker <gitlab@zwicker.io>
2023-08-07 21:43:40 +0200

    Add Json Header

--- Sources/ZNetwork/Enumerates/ZNHeader.swift
+++ Sources/ZNetwork/Enumerates/ZNHeader.swift
@@ -7,17 +7,20 @@
 
 public enum ZNHeader {
     case Cookie
+    case ContentJson
 }
 
 extension ZNHeader {
     var key: String {
         switch self {
         case .Cookie: return "Cookie"
+        case .ContentJson: return "Content-Type"
         }
     }
 
     var value: String {
         switch self {
+        case .ContentJson: return "application/json"
         default: return ""
         }
     }
