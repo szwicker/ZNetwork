41ee72723edc252f52f45a7f79fe729f3c0ccb77
Simon Zwicker <gitlab@zwicker.io>
2023-08-07 10:53:20 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ZNetwork
+
+A description of this package.
---
+++ Tests/ZNetworkTests/ZNetworkTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ZNetwork
+
+final class ZNetworkTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ZNetwork().text, "Hello, World!")
+    }
+}
