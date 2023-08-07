399c9a869a4091987c31d906dd61205a8f6cb2f9
Simon Zwicker <gitlab@zwicker.io>
2023-08-07 23:00:24 +0200

    Init Components

--- Sources/ZNetwork/ZNetworkComponent.swift
+++ Sources/ZNetwork/ZNetworkComponent.swift
@@ -9,4 +9,10 @@ public struct ZNetworkComponent {
     var scheme: String
     var host: String
     var path: String
+
+    public init(scheme: String, host: String, path: String) {
+        self.scheme = scheme
+        self.host = host
+        self.path = path
+    }
 }
