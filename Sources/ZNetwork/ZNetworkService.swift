d0292ca7d1961d961b9bb494f863e30e46853120
Simon Zwicker <gitlab@zwicker.io>
2023-08-12 14:57:31 +0200

    Decode empty response

--- Sources/ZNetwork/ZNetworkService.swift
+++ Sources/ZNetwork/ZNetworkService.swift
@@ -69,9 +69,7 @@ extension ZNetworkService {
             ZNetwork.logger.log(response, data: data)
             switch response.statusCode {
             case 204:
-                guard let emptyResponse = try? JSONDecoder().decode(T.self, from: Data()) else {
-                    return .failure(.decode)
-                }
+                let emptyResponse = try! JSONDecoder().decode(T.self, from: Data())
                 return .success(emptyResponse)
             case 200...299:
                 guard let decodedResponse = try? JSONDecoder().decode(T.self, from: data) else {
